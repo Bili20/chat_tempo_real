6056306aa0548f37e340954e12d2e27b97f69509
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-10-06 16:59:05 -0300

    fix: app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import { serverHttp } from "./config/server";
 
-serverHttp.listen(3004, () => {
-  console.log("Servidor rodadndo http://localhost:3004");
+serverHttp.listen(3005, () => {
+  console.log("Servidor rodadndo http://localhost:3005");
 });
