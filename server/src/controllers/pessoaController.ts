e29e7ed8a9ac290eba5d28625246c2ed652912b0
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-10-06 17:13:37 -0300

    fix: pessoa controller

--- server/src/controllers/pessoaController.ts
+++ server/src/controllers/pessoaController.ts
@@ -1,1 +1,22 @@
-export class PessoaController {}
+import { Request, Response } from "express";
+import { prismaClient } from "../config/prismaClient";
+import bcrypt from "bcrypt";
+export class PessoaController {
+  static async createPessoa(req: Request, res: Response) {
+    let { nome, email, senha, cpf } = req.body;
+    try {
+      senha = await bcrypt.hash(senha, 10);
+      const pessoa = await prismaClient.pessoa.create({
+        data: {
+          nome,
+          email,
+          senha,
+          cpf,
+        },
+      });
+      res.status(201).json({ id: pessoa.id, nome: pessoa.nome });
+    } catch (e) {
+      res.status(500).json({ message: `${e} - falha ao cadastrar` });
+    }
+  }
+}
