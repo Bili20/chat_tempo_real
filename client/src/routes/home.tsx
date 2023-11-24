d445ab441c02edfc0a583d1ab46740a51a5b92da
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-11-24 17:37:20 -0300

    fix: home
    executando uma requisição

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,7 +1,27 @@
-import React from "react";
 import NavBar from "../components/navbarComponents/navBar";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [user, setUser] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("http://192.168.155.19:3002/pessoa");
+
+      const data = response.data;
+
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return <NavBar />;
 };
 
