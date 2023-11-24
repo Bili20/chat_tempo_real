747fd580fb5b5b490a2a576b415a8c9a4a8f125c
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-11-24 08:42:41 -0300

    env da porta

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import { serverHttp } from "./config/server";
 
-serverHttp.listen(3005, () => {
-  console.log("Servidor rodadndo http://localhost:3005");
+serverHttp.listen(process.env.PORTA, () => {
+  console.log(`Servidor rodadndo http://localhost:${process.env.PORTA}`);
 });
