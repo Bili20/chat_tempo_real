9c614e60a35f639e0b0df1f8f0e85aff76367ed7
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-11-27 16:25:52 -0300

    fix: drop box

---
+++ client/src/components/homeComponents/dropBox.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { pessoas } from "../../routes/home";
+
+interface IProps {
+  idGrupo: number;
+  grupoNome: string;
+  users: pessoas[];
+}
+
+const DropDown = (props: IProps) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <div className="btn-grupos">
+        <button onClick={() => setOpen((old) => !old)}>
+          {props.grupoNome}
+        </button>
+        <button className="conversa" onClick={}>
+          <img className="icone" src="../../../img/comente.png" alt="logo" />
+        </button>
+      </div>
+      {open
+        ? props.users.map((user) => {
+            const filtro = user.grupos_pessoas.filter(
+              (item) => item.grupo.id === props.idGrupo
+            );
+            return (
+              <ul className="menu">
+                {filtro.length > 0 ? (
+                  <li className="menu-item" key={user.id}>
+                    <h2>{user.nome}</h2>
+                  </li>
+                ) : null}
+              </ul>
+            );
+          })
+        : null}
+    </>
+  );
+};
+
+export default DropDown;
