d87ec67e9132a56190a65757de29c809fb7b2257
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-11-27 16:25:38 -0300

    fix: config css geral

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
 import { Outlet } from "react-router-dom";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <Outlet />
-      </div>
-    </div>
-  );
+  return <Outlet />;
 }
 
 export default App;
