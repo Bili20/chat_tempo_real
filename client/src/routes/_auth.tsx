6ef1be377fdf6a33d6a35d60df70a68803c19329
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-11-29 13:51:07 -0300

    fix: troca de pasta

--- client/src/routes/_auth.tsx
+++
@@ -1,7 +0,0 @@
-import { Outlet } from "react-router-dom";
-import Login from "./login";
-
-export default function AuthRouter() {
-  const isConnect = true;
-  return isConnect ? <Outlet /> : <Login />;
-}
