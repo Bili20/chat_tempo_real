0ecb8eb34734a8c88d23878bdd09fa7e684948f8
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-01 18:00:08 -0300

    Update indexRoutes.ts

--- server/src/routes/indexRoutes.ts
+++ server/src/routes/indexRoutes.ts
@@ -2,13 +2,22 @@ import express, { Request, Response } from "express";
 import { pessoaRoutes } from "./pessoaRoutes";
 import { authRoutes } from "./authRoutes";
 import { grupoRoutes } from "./grupoRoutes";
+import { conversaRoutes } from "./conversaRoutes";
+import { mensagemGrupoRoutes } from "./mensagemGrupoRoutes";
 
 const routes = (app: any) => {
   app
     .route("/")
     .get((req: Request, res: Response) => res.status(200).send("Funcionando"));
 
-  app.use(express.json(), pessoaRoutes, authRoutes, grupoRoutes);
+  app.use(
+    express.json(),
+    pessoaRoutes,
+    authRoutes,
+    grupoRoutes,
+    conversaRoutes,
+    mensagemGrupoRoutes
+  );
 };
 
 export { routes };
