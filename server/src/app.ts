e574b2f7aafa9105f81c8a187cdbc642d81121e1
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-01 17:59:36 -0300

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,9 @@
 import { serverHttp } from "./config/server";
+import { io } from "./config/webSocket";
+
+io.on("connection", (socket) => {
+  console.log("Usuario conectado:", socket.id);
+});
 
 serverHttp.listen(process.env.PORTA, () => {
   console.log(`Servidor rodadndo http://localhost:${process.env.PORTA}`);
