364ac9e762065e61fa165e40f45ecefe9a67b599
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-04 16:44:01 -0300

    Update telaConversa.tsx

--- client/src/routes/telaConversa/telaConversa.tsx
+++ client/src/routes/telaConversa/telaConversa.tsx
@@ -1,24 +1,83 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./style/telaConversa.css";
 import NavBar from "../../components/navbarComponents/navBar";
+import { useParams } from "react-router-dom";
+import { webFetch } from "../../axios/axiosConfig";
+import { AuthContext } from "../../contexts/auth/authContext";
+
+type grupo = {
+  id: number;
+  nome: string;
+  descricao: string;
+};
+
+type mensagens = {
+  id: number;
+  mensagem: string;
+  dataCadastro: Date;
+  idConversa: number;
+  idPessoa: number;
+}[];
 
 const TelaConversa = () => {
-  const [menssagemBox, setMenssagemBox] = useState();
+  const [mensagensBox, setMensagensBox] = useState<mensagens>([]);
+  const [value, setGrupo] = useState<grupo>();
+  const { idGrupo } = useParams();
+
+  // preciso buscar as informaçoes do grupo, criar o type
+  const getGrupo = async () => {
+    try {
+      const grupo = await webFetch.get(`/grupo/${idGrupo}`);
+
+      setGrupo(grupo.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  // preciso buscar as mensagens da conversa
+  // preciso diferenciar oq é mensagem do usuario e oq não é
+  const getMensagens = async () => {
+    const storageData = localStorage.getItem("AuthAccess");
+    try {
+      const mensagens = await webFetch.post("/conversa", {
+        access: storageData,
+        idGrupo: Number(idGrupo),
+      });
+      console.log(mensagens);
+      setMensagensBox(mensagens.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // preciso enviar mensagem
+
+  useEffect(() => {
+    getGrupo();
+    getMensagens();
+  }, []);
 
   return (
     <>
       <NavBar />
       <div className="titulo">
-        <h2>Nome do grupo</h2>
+        <h2>{value?.nome}</h2>
       </div>
       <div className="modal">
+        {mensagensBox.map((value) => {
+          return (
+            <div key="texto" className="texto">
+              <ul key="mensagens">
+                <p className="user">nome user</p>
+                <li key="mensagem">{value.mensagem}</li>
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+      <div>
         <input className="caixa-digito" type="text" placeholder="Digite aqui" />
-        <div className="conteudo">
-          <div className="texto">
-            <p className="nome-user">nome user</p>
-            <p>oi estou no conteudo</p>
-          </div>
-        </div>
+        <button className="envia-mensagem">enviar</button>
       </div>
     </>
   );
