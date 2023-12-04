908e8dda58a5bf3bd02ca9790d82a0b493c023a2
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-04 10:47:35 -0300

    Update mensagemGrupoController.ts

--- server/src/controllers/mensagemGrupo/mensagemGrupoController.ts
+++ server/src/controllers/mensagemGrupo/mensagemGrupoController.ts
@@ -3,22 +3,26 @@ import { IMensagemGrupo } from "./interface/interfaceMensagemGrupo";
 import { prismaClient } from "../../config/prismaClient";
 
 export class MensagemGrupoController {
-  static async createMensagem(res: Response, req: Request) {
+  static async createMensagem(req: Request, res: Response) {
     const IMensagemGrupo: IMensagemGrupo = req.body;
-    console.log(IMensagemGrupo);
-    // verificar se ele pode mandar mensagem no grupo
     try {
-      /* const mensagem = await prismaClient.mensagem_grupo.create({
+      await prismaClient.mensagem_grupo.create({
         data: {
           id_pessoa: IMensagemGrupo.idPessoa,
           id_conversa: IMensagemGrupo.idConversa,
           mensagem: IMensagemGrupo.mensagem,
         },
-      }); */
+      });
+      res.status(200).json();
     } catch (e) {
       throw new Error(e);
     }
   }
 
-  static async findMensagemUser() {}
+  static async findMensagemUser(idConversa: number) {
+    const mensagens = await prismaClient.mensagem_grupo.findMany({
+      where: { id_conversa: idConversa },
+    });
+    return mensagens;
+  }
 }
