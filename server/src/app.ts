4b0945c6b02a08d3882024f293220a5c8f839510
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-06 14:22:06 -0300

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import { serverHttp } from "./config/server";
 import { io } from "./config/webSocket";
 
-io.on("connection", (socket) => {
+/* io.on("connection", (socket) => {
   console.log("Usuario conectado:", socket.id);
 
   socket.on("disconnect", () => {
@@ -11,7 +11,7 @@ io.on("connection", (socket) => {
   socket.on("user", (user) => {
     socket.data.username = user;
   });
-});
+}); */
 
 serverHttp.listen(process.env.PORTA, () => {
   console.log(`Servidor rodadndo http://localhost:${process.env.PORTA}`);
