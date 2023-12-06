1944f7ba7dea5e5d3acb378fd07489277b024b0a
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-06 14:22:17 -0300

    Update conversaRoutes.ts

--- server/src/routes/conversaRoutes.ts
+++ server/src/routes/conversaRoutes.ts
@@ -5,9 +5,15 @@ import { ConversaController } from "../controllers/conversa/conversaController";
 const conversaRoutes = express.Router();
 
 conversaRoutes.post(
-  "/conversa",
+  "/conversa/grupo",
   AuthController.verificaJWt,
   ConversaController.findConversaUserGrupo
+);
+
+conversaRoutes.post(
+  "/conversa/privada",
+  AuthController.verificaJWt,
+  ConversaController.findConversaUserPrivada
 );
 
 export { conversaRoutes };
