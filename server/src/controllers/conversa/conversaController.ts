4d2acf0841dc205d0bbc2f81fb8b4f1d372de989
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-06 14:22:08 -0300

    Update conversaController.ts

--- server/src/controllers/conversa/conversaController.ts
+++ server/src/controllers/conversa/conversaController.ts
@@ -2,8 +2,10 @@ import { Request, Response } from "express";
 import { AuthController } from "../auth/authController";
 import { prismaClient } from "../../config/prismaClient";
 import { PessoaController } from "../pessoa/pessoaController";
-import { IConversaGrupo } from "./interface/interfaceConversa";
-import { MensagemGrupoController } from "../mensagemGrupo/mensagemGrupoController";
+import {
+  IConversaGrupo,
+  IConversaPrivada,
+} from "./interface/interfaceConversa";
 
 export class ConversaController {
   static async initConversaGrupo(
@@ -27,7 +29,7 @@ export class ConversaController {
       const pessoa = await PessoaController.findOnePessoa(user.id);
 
       const conversa = await prismaClient.conversa.findFirst({
-        where: { id_pessoa: pessoa.id, id_grupo: IConversa.idGrupo },
+        where: { id_grupo: IConversa.idGrupo },
       });
 
       if (conversa == null) {
@@ -35,15 +37,55 @@ export class ConversaController {
           pessoa.id,
           IConversa.idGrupo
         );
-        const mensagens = await MensagemGrupoController.findMensagemUser(
-          IConversa.idGrupo
-        );
-        res.status(200).json(mensagens);
+        res.status(200).json(conversa);
       } else {
-        const mensagens = await MensagemGrupoController.findMensagemUser(
-          IConversa.idGrupo
+        res.status(200).json(conversa);
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  static async initConversaPrivada(idEmissor: number, idReceptor: number) {
+    try {
+      const conversa = await prismaClient.conversa.create({
+        data: { id_pessoa: idEmissor, id_receptor: idReceptor },
+      });
+      return conversa;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  static async findConversaUserPrivada(req: Request, res: Response) {
+    const IConversa: IConversaPrivada = req.body;
+    try {
+      const user = await AuthController.currentUser(IConversa.access);
+      const pessoa = await PessoaController.findOnePessoa(user.id);
+
+      const conversa = await prismaClient.conversa.findFirst({
+        where: {
+          OR: [
+            {
+              id_pessoa: pessoa.id,
+              id_receptor: IConversa.idReceptor,
+            },
+            {
+              id_receptor: pessoa.id,
+              id_pessoa: IConversa.idReceptor,
+            },
+          ],
+        },
+      });
+      if (conversa == null) {
+        const conversa = await ConversaController.initConversaPrivada(
+          pessoa.id,
+          IConversa.idReceptor
         );
-        res.status(200).json(mensagens);
+
+        res.status(200).json(conversa);
+      } else {
+        res.status(200).json(conversa);
       }
     } catch (e) {
       throw new Error(e);
