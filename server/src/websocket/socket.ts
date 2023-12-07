7b4207ac2e53b9a627c0593b01d478208108a97a
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-07 16:36:39 -0300

    Create socket.ts

---
+++ server/src/websocket/socket.ts
@@ -0,0 +1,40 @@
+import { io } from "../config/webSocket";
+import { MensagemGrupoController } from "../controllers/mensagemGrupo/mensagemGrupoController";
+
+interface IuserRoom {
+  nome: string;
+  grupo: number;
+  socketId: string;
+}
+
+const users: IuserRoom[] = [];
+
+io.on("connection", (socket) => {
+  socket.on("room", (data) => {
+    socket.join(data.grupo);
+
+    const userInRoom = users.find(
+      (user) => user.nome === data.nome && user.grupo === data.grupo
+    );
+    if (userInRoom) {
+      userInRoom.socketId = socket.id;
+    } else {
+      users.push({
+        nome: data.nome,
+        grupo: data.grupo,
+        socketId: socket.id,
+      });
+    }
+    console.log(users);
+  });
+
+  socket.on("mensagem", async (data) => {
+    const mensagem = await MensagemGrupoController.testecreateMensagem(data);
+
+    io.to(data.idConversa).emit("mensagem", mensagem);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Usuario desconectado", socket.id);
+  });
+});
