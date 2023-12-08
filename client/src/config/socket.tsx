cf0dabe5808df6ac284812a1138ea1e27a2f48bf
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-08 11:57:12 -0300

    Update socket.tsx

--- client/src/config/socket.tsx
+++ client/src/config/socket.tsx
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3002", { autoConnect: false });
+export const socket = io("http://192.168.155.19:3002", { autoConnect: false });
