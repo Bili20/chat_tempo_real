c28706f52a5a933b82ef466d5e3cf9471701e871
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-08 11:57:20 -0300

    Update socket.ts

--- server/src/websocket/socket.ts
+++ server/src/websocket/socket.ts
@@ -1,36 +1,76 @@
 import { io } from "../config/webSocket";
 import { MensagemGrupoController } from "../controllers/mensagemGrupo/mensagemGrupoController";
+import { MensagemPrivadaController } from "../controllers/mensagemPrivada/mensagemPrivadaController";
 
 interface IuserRoom {
-  nome: string;
+  idUser: number;
   grupo: number;
   socketId: string;
 }
 
-const users: IuserRoom[] = [];
+const usersGrupo: IuserRoom[] = [];
+
+interface IUserPrivado {
+  idUser: number;
+  idReceptor: number;
+  idConversa: number;
+  socketId: string;
+}
+
+const usersPrivado: IUserPrivado[] = [];
 
 io.on("connection", (socket) => {
   socket.on("room", (data) => {
     socket.join(data.grupo);
 
-    const userInRoom = users.find(
-      (user) => user.nome === data.nome && user.grupo === data.grupo
+    const userInRoom = usersGrupo.find(
+      (user) => user.idUser === data.idUser && user.grupo === data.grupo
     );
     if (userInRoom) {
       userInRoom.socketId = socket.id;
     } else {
-      users.push({
-        nome: data.nome,
+      usersGrupo.push({
+        idUser: data.idUser,
         grupo: data.grupo,
         socketId: socket.id,
       });
     }
   });
 
-  socket.on("mensagem", async (data) => {
-    const mensagem = await MensagemGrupoController.testecreateMensagem(data);
+  socket.on("mensagemGrupo", async (data) => {
+    const mensagem = await MensagemGrupoController.testecreateMensagemGrupo(
+      data
+    );
 
-    io.to(data.idConversa).emit("mensagem", mensagem);
+    io.to(data.idConversa).emit("mensagemGrupo", mensagem);
+  });
+
+  socket.on("userReceptor", (data) => {
+    socket.join(data.idConversa);
+
+    const userLogged = usersPrivado.find((user) => user.idUser === data.idUser);
+    if (userLogged) {
+      userLogged.socketId = socket.id;
+    } else {
+      usersPrivado.push({
+        idUser: data.idUser,
+        idReceptor: data.idReceptor,
+        idConversa: data.idConversa,
+        socketId: socket.id,
+      });
+    }
+  });
+
+  socket.on("mensagemPrivada", async (data) => {
+    const mensagem = await MensagemPrivadaController.testecreateMensagemPrivada(
+      {
+        idConversa: data.idConversa,
+        idPessoa: data.idPessoa,
+        mensagem: data.mensagem,
+      }
+    );
+
+    io.to([data.idConversa]).emit("mensagemPrivada", mensagem);
   });
 
   socket.on("disconnect", () => {
