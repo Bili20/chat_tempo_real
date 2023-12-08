7e119b2281c5d730c40e282649b04a8af48e74f2
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-08 17:56:56 -0300

    Update login.tsx

--- client/src/routes/login/login.tsx
+++ client/src/routes/login/login.tsx
@@ -27,36 +27,49 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={logar}>
-        <div className="form-control">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            name="e-mail"
-            id="email"
-            value={payload.email}
-            onChange={(e) =>
-              setPayload((old) => ({ ...old, email: e.target.value }))
-            }
-            placeholder="Digite seu e-mail"
-          />
+    <div className="container">
+      <div className="form-img">
+        <img className="user-logo" src="../../../img/userLogo.png" alt="" />
+      </div>
+      <div className="form-login">
+        <div className="container-titulo">
+          <img className="user-name" src="../../../img/username.png" alt="" />
         </div>
-        <div className="form-control">
-          <label htmlFor="senha">Senha:</label>
-          <input
-            type="password"
-            name="senha"
-            id="senha"
-            value={payload.senha}
-            onChange={(e) =>
-              setPayload((old) => ({ ...old, senha: e.target.value }))
-            }
-            placeholder="Digite sua senha"
-          />
-        </div>
-        <button type="submit">Entrar</button>
-      </form>
+        <form onSubmit={logar}>
+          <div className="form-control">
+            <label htmlFor="email">Email:</label>
+            <input
+              className="input-login"
+              type="text"
+              name="e-mail"
+              id="email"
+              value={payload.email}
+              onChange={(e) =>
+                setPayload((old) => ({ ...old, email: e.target.value }))
+              }
+              placeholder="Digite seu e-mail"
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="senha">Senha:</label>
+            <input
+              className="input-login"
+              type="password"
+              name="senha"
+              id="senha"
+              value={payload.senha}
+              onChange={(e) =>
+                setPayload((old) => ({ ...old, senha: e.target.value }))
+              }
+              placeholder="Digite sua senha"
+            />
+          </div>
+          <button className="button-login" type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
